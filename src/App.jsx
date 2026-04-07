0976616baf4ede504c71f4348354e0839776e307
Themton <themton@users.noreply.github.com>
2026-04-07 15:59:41 +0000

    chore: configure for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import SalePage from './components/SalePage';
 import AdminDashboard from './components/AdminDashboard';
 import PageEditor from './components/PageEditor';
@@ -6,13 +6,13 @@ import './index.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/:pageId" element={<PageEditor />} />
         <Route path="/:slug" element={<SalePage />} />
         <Route path="/" element={<AdminDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
